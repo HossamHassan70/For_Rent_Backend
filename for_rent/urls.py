435c7186e200f016fdbab1df92b99ebe3c2fba85
Mikerosoft99 <kerosamy8000@gmail.com>
2024-03-12 13:32:08 +0200

    Added reviews_api to urls

--- for_rent/urls.py
+++ for_rent/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from reviews_api.views import (
+    ReviewDetail,
+    ReviewList,
+)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('reviews/', ReviewList.as_view(), name='review-list'),
+    path('reviews/<int:pk>/', ReviewDetail.as_view(), name='review-detail'),
 ]
