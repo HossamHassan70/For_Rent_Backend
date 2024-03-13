c63d30e56bb46ea5a15246bc2e9637d317168c85
Mikerosoft99 <kerosamy8000@gmail.com>
2024-03-13 02:46:26 +0200

    Added constrains on rating

--- reviews_api/models.py
+++ reviews_api/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Review(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    rating = models.IntegerField()
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=255)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)]
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
