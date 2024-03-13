d6b7565bca5673f498c9d34e5c25c3dc2eb8da9d
Mikerosoft99 <kerosamy8000@gmail.com>
2024-03-13 21:24:24 +0200

    Edited model relation

--- reviews_api/models.py
+++ reviews_api/models.py
@@ -5,12 +5,12 @@ from property_api.models import Property
 
 class Review(models.Model):
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    property = models.ForeignKey(Property, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     content = models.TextField(max_length=255)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
