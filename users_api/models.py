cf52001346cb31c866e9dfd5a4a595324237db1d
Mikerosoft99 <kerosamy8000@gmail.com>
2024-03-14 17:01:20 +0200

    Added property relation to user model

--- users_api/models.py
+++ users_api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from property_api.models import Property
+
 
 class Role(models.TextChoices):
     RENTER = "Renter", "Renter"
@@ -15,3 +17,12 @@ class User(models.Model):
     role = models.CharField(max_length=7, choices=Role.choices, default=Role.RENTER)
     validation_states = models.BooleanField(default=False)
     registration_date = models.DateTimeField(auto_now_add=True)
+    properties = models.ManyToManyField(
+        Property, related_name="rented_by", through="UserPropertyRole"
+    )
+
+
+class UserPropertyRole(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    role = models.CharField(max_length=7, choices=Role.choices, default=Role.RENTER)
