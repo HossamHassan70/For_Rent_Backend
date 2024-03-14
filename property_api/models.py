8705d88be41f6616646a94a8466fcdbcbe3d1325
Mikerosoft99 <kerosamy8000@gmail.com>
2024-03-14 17:24:00 +0200

    Added user relation to property_api model

--- property_api/models.py
+++ property_api/models.py
@@ -1,22 +1,23 @@
 from django.db import models
-#from django.contrib.auth.models import User
 
 class Property(models.Model):
-   #Owner = models.ForeignKey(User, related_name='owned_properties', on_delete=models.CASCADE)
-   #Renters = models.ManyToManyField(User, related_name='rented_properties')
-    ST_No = models.CharField(max_length=10)
-    Building_No = models.CharField(max_length=10)
-    Type = models.CharField(max_length=50)
-    Address = models.TextField()
-    Availability = models.BooleanField(default=True)
-    Price = models.DecimalField(max_digits=10, decimal_places=2)
-    Image = models.ImageField(upload_to='static/property_images/')
-    Title = models.CharField(max_length=200)
-    Description = models.TextField()
-    Rooms = models.IntegerField()
-    Bathrooms = models.IntegerField()
-
-
-    def __str__(self):
-     return self.Title
+    PROPERTY_TYPES = [
+        ('house', 'House'),
+        ('apartment', 'Apartment'),
+        ('condo', 'Condo'),
+        ('villa', 'Villa'),
+    ]
     
+    type = models.CharField(max_length=50, choices=PROPERTY_TYPES, default='house')
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    address = models.TextField()
+    availability = models.BooleanField(default=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='static/property_images/')
+    rooms = models.IntegerField()
+    bathrooms = models.IntegerField()
+    owner = models.ForeignKey('User', on_delete=models.CASCADE, related_name='owned_properties', null=True, blank=True)
+
+    def str(self):
+        return self.title
