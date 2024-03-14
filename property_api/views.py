97967c4c7aeabf80a10e7346006f168d56f6d6a0
amany-abdelrahman <amanyabdelrahmansalah0@gmail.com>
2024-03-14 03:37:29 +0200

    add search on property_api

--- property_api/views.py
+++ property_api/views.py
@@ -14,27 +14,22 @@ class PropertyClassViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = Property.objects.all()
 
-        # Filter by type
         type_param = self.request.query_params.get('type', None)
         if type_param:
             queryset = queryset.filter(Type=type_param)
 
-        # Filter by price
         price_param = self.request.query_params.get('price', None)
         if price_param:
             queryset = queryset.filter(Price=price_param)
 
-        # Filter by rooms
         rooms_param = self.request.query_params.get('rooms', None)
         if rooms_param:
             queryset = queryset.filter(Rooms=rooms_param)
-        
-        # Filter by bathrooms
+
         bathrooms_param = self.request.query_params.get('bathrooms', None)
         if bathrooms_param:
             queryset = queryset.filter(Bathrooms=bathrooms_param)
 
-        # Filter by address
         address_param = self.request.query_params.get('address', None)
         if address_param:
             queryset = queryset.filter(Address__icontains=address_param)
