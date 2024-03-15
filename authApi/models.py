ba3e5621bc41574845d3b0a68d05787a8bdc7f8e
Mikerosoft99 <kerosamy8000@gmail.com>
2024-03-15 18:35:10 +0200

    Deleted auth model & serializer

--- authApi/models.py
+++ authApi/models.py
@@ -1,11 +1,2 @@
 from django.db import models
 
-
-class authi(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=20)
-    password = models.CharField(max_length=16)
-    fullname = models.CharField(max_length=20)
-    username = models.CharField(max_length=20)
-    roles = (("r", "renter"), ("o", "owner"))
-    role = models.CharField(choices=roles, max_length=1)
--- authApi/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from authApi.models import authi
-
-
-class AuthiSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = authi
-        fields = '__all__'
