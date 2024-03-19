a297bd18720bfcfd575428fb812dccd782b5f1de
amany-abdelrahman <111478505+amany-abdelrahman@users.noreply.github.com>
2024-03-19 20:27:38 +0200

    Update queryset in property_api

--- property_api/views.py
+++ property_api/views.py
@@ -2,7 +2,6 @@ from property_api.serializers import PropertySerializer
 from property_api.models import Property
 from rest_framework import viewsets
 
-
 class PropertyClassViewSet(viewsets.ModelViewSet):
     serializer_class = PropertySerializer
     queryset = Property.objects.all()
@@ -10,29 +9,24 @@ class PropertyClassViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         queryset = Property.objects.all()
 
-        # Filter by type
-        type_param = self.request.query_params.get("type", None)
+        type_param = self.request.query_params.get('type', None)
         if type_param:
-            queryset = queryset.filter(Type=type_param)
+            queryset = queryset.filter(type=type_param)
 
-        # Filter by price
-        price_param = self.request.query_params.get("price", None)
+        price_param = self.request.query_params.get('price', None)
         if price_param:
-            queryset = queryset.filter(Price=price_param)
+            queryset = queryset.filter(price=price_param)
 
-        # Filter by rooms
-        rooms_param = self.request.query_params.get("rooms", None)
+        rooms_param = self.request.query_params.get('rooms', None)
         if rooms_param:
-            queryset = queryset.filter(Rooms=rooms_param)
-
-        # Filter by bathrooms
-        bathrooms_param = self.request.query_params.get("bathrooms", None)
+            queryset = queryset.filter(rooms=rooms_param)
+        
+        bathrooms_param = self.request.query_params.get('bathrooms', None)
         if bathrooms_param:
-            queryset = queryset.filter(Bathrooms=bathrooms_param)
+            queryset = queryset.filter(bathrooms=bathrooms_param)
 
-        # Filter by address
-        address_param = self.request.query_params.get("address", None)
+        address_param = self.request.query_params.get('address', None)
         if address_param:
-            queryset = queryset.filter(Address__icontains=address_param)
+            queryset = queryset.filter(address__icontains=address_param)
 
         return queryset
