662f02c09b7d0c99e847da7624332279f883676a
kerollossamy <kerollossamy9908@gmail.com>
2024-03-19 22:36:47 +0200

    Changed view to viewSet

--- payment_api/models.py
+++ payment_api/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from users_api.models import User
+
 
 class Payment(models.Model):
-    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     timestamp = models.DateTimeField(auto_now_add=True)
     is_successful = models.BooleanField(default=False)
--- payment_api/views.py
+++ payment_api/views.py
@@ -1,12 +1,9 @@
-from rest_framework import generics
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Payment
 from .serializers import PaymentSerializer
 
-class PaymentListCreateAPIView(generics.ListCreateAPIView):
+
+class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
-    permission_classes = [IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
