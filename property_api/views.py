63e76e70312020454a5394fcb384598d38154cdc
Hossam Hassan <99551852+HossamHassan70@users.noreply.github.com>
2024-03-23 16:13:24 +0200

    Fix Search Functionality and Test it

--- property_api/views.py
+++ property_api/views.py
@@ -1,26 +1,22 @@
 from property_api.serializers import PropertySerializer
 from property_api.models import Property
 from rest_framework import viewsets, filters
-from decimal import Decimal
 
 
 class PropertyClassViewSet(viewsets.ModelViewSet):
     serializer_class = PropertySerializer
-    queryset = Property.objects.all()
     filter_backends = [filters.SearchFilter]
-    search_fields = ["title", "description", "address"]
 
     def get_queryset(self):
         queryset = Property.objects.all()
 
+        title_param = self.request.query_params.get("title", None)
+        if title_param:
+            queryset = queryset.filter(title__icontains=title_param)
+
         type_param = self.request.query_params.get("type", None)
         if type_param:
             queryset = queryset.filter(type=type_param)
-
-        price_param = self.request.query_params.get("price", None)
-        if price_param:
-            price_param = Decimal(price_param)
-            queryset = queryset.filter(price=price_param)
 
         rooms_param = self.request.query_params.get("rooms", None)
         if rooms_param:
@@ -29,9 +25,5 @@ class PropertyClassViewSet(viewsets.ModelViewSet):
         bathrooms_param = self.request.query_params.get("bathrooms", None)
         if bathrooms_param:
             queryset = queryset.filter(bathrooms=bathrooms_param)
-
-        address_param = self.request.query_params.get("address", None)
-        if address_param:
-            queryset = queryset.filter(address__icontains=address_param)
 
         return queryset
