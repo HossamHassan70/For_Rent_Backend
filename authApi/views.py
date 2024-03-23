3da791840f0e837975390cb74befe8c8659921cb
kerollossamy <kerollossamy9908@gmail.com>
2024-03-23 07:24:42 +0200

    Created end point for verifying code

--- authApi/views.py
+++ authApi/views.py
@@ -1,105 +1,114 @@
-from django.shortcuts import render
 import jwt
 from rest_framework import viewsets, status
-# from authApi.models import authi
-# from authApi.serializers import AuthiSerializer
-from django.contrib.auth import authenticate, login
-from django.http import JsonResponse
 from rest_framework.views import APIView
-from rest_framework.decorators import action
 from rest_framework.response import Response
-from django.contrib.auth import get_user_model
-from django.contrib.auth.hashers import check_password
 from for_rent import settings
 from users_api.models import User
 from users_api.serializers import UserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
-from datetime import timedelta
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from django.contrib.auth import get_user_model
 from .models import UserEmailVerification
+
 
 # Create your views here.
 class authiView(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    serializer_class =  UserSerializer
+    serializer_class = UserSerializer
 
 
 class LoginView(APIView):
     def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
+        username = request.data.get("username")
+        password = request.data.get("password")
 
         if username is None or password is None:
-            return Response({'error': 'Please provide both username and password'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "Please provide both username and password"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
         try:
             user = User.objects.get(username=username)
         except User.DoesNotExist:
-            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response(
+                {"error": "Invalid Credentials"}, status=status.HTTP_401_UNAUTHORIZED
+            )
 
-        if user.password != password: 
-            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        if user.password != password:
+            return Response(
+                {"error": "Invalid Credentials"}, status=status.HTTP_401_UNAUTHORIZED
+            )
 
-        serializer = UserSerializer(user)  
+        serializer = UserSerializer(user)
 
-        #pay load hna
+        # pay load hna
         user_data = {
-            'id': user.id,
-            'username': user.username,
-            'email': user.email,
-            'phoneNumber': str(user.phone_number),
-            'profilePicture' : user.get_profile_picture_url(),
-            'role': user.role,
-            'registration_date':str(user.registration_date),
-            'validation_states':bool()
-            # infos zyada
+            "id": user.id,
+            "username": user.username,
+            "email": user.email,
+            "phoneNumber": str(user.phone_number),
+            "profilePicture": user.get_profile_picture_url(),
+            "role": user.role,
+            "registration_date": str(user.registration_date),
+            "validation_states": user.validation_states,
         }
 
-        # JWT 
+        # JWT
         refresh = RefreshToken.for_user(user)
         # refresh.set_exp(lifetime=timedelta(hours=1))
 
         # refresh tk
-        refresh['user'] = user_data
+        refresh["user"] = user_data
 
-        return Response({
-            # 'user': serializer.data,
-            'refresh': str(refresh),
-            'access': str(refresh.access_token),
-        }, status=status.HTTP_200_OK)
-    
+        return Response(
+            {
+                # 'user': serializer.data,
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            },
+            status=status.HTTP_200_OK,
+        )
 
 
-    
-class VerifyEmailToken(APIView):
+class VerifyCode(APIView):
     authentication_classes = [JWTAuthentication]
 
     def post(self, request):
-        token = request.headers.get('Authorization').split(' ')[1]  # Extract JWT token from Authorization header
-        decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=['HS256'])  # Decode JWT token
+        # Extract JWT token from Authorization header
+        token = request.headers.get("Authorization").split(" ")[1]
+        # verify and decode token
+        decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+        print(f"this is decoded Token {decoded_token}")
 
-        email = decoded_token.get('email')  # Extract email from decoded token
+        # get email decoded
+        email = decoded_token.get("user").get(
+            "email"
+        )  # Extract email from decoded token
+        print(f"this is user email {email}")
 
+        # 3 search for UserEmailVerification with matching email
         try:
             user_verification = UserEmailVerification.objects.get(email=email)
+            user = User.objects.get(email=email)
         except UserEmailVerification.DoesNotExist:
-            return Response({'error': 'No verification record found for this email'}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {"error": "No verification record found for this email"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
 
         # Compare code in request with code in UserEmailVerification instance
-        if user_verification.code == request.data.get('code'):
-            user_verification.verified = True
-            user_verification.save()
-            return Response({'message': 'Email verification successful'}, status=status.HTTP_200_OK)
+        if str(user_verification.code) == str(request.data.get("code")):
+            # if matching, change states to true
+            user.validation_states = True
+            user.save()
+            return Response(
+                {"message": "Email verification successful"}, status=status.HTTP_200_OK
+            )
         else:
-            return Response({'error': 'Invalid verification code'}, status=status.HTTP_400_BAD_REQUEST)
-
-#create end point
-    #1 verify and decode token
-    #2 get email decoded
-    #3 search for UserEmailVerification with matching email
-    #4 compare code in request with code in UserEmailVerification instance
-    #5 if mathcing, change states to true 
+            return Response(
+                {"error": "Invalid verification code"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
