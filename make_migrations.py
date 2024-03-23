4c3497bad34be24e44a3e5c17a95b2127079d5fa
kerollossamy <kerollossamy9908@gmail.com>
2024-03-23 20:56:28 +0200

    Customized python commands for make_migrations file

--- make_migrations.py
+++ make_migrations.py
@@ -1,17 +1,21 @@
 import subprocess
+import sys
 
 commands = [
-    "python3 manage.py makemigrations property_api",
-    "python3 manage.py makemigrations reviews_api",
-    "python3 manage.py makemigrations users_api",
-    "python3 manage.py makemigrations authApi",
-    "python3 manage.py makemigrations requests_api",
-    "python3 manage.py makemigrations payment_api",
-    "python3 manage.py migrate"
+    "manage.py makemigrations property_api",
+    "manage.py makemigrations reviews_api",
+    "manage.py makemigrations users_api",
+    "manage.py makemigrations authApi",
+    "manage.py makemigrations requests_api",
+    "manage.py makemigrations payment_api",
+    "manage.py migrate",
 ]
+
+# Determine which Python launcher to use based on the platform
+python_launcher = "python3" if sys.platform != "win32" else "py"
 
 for command in commands:
     print(f"Executing command: {command}")
-    subprocess.run(command, shell=True)
+    subprocess.run(f"{python_launcher} {command}", shell=True)
 
 print("All commands executed successfully.")
